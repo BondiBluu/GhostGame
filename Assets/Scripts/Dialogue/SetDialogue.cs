9f86aba5b8a0a8a21175dc1f3d2b19ea6ab0b4c0
Bami O. <bamideleogundeji949@gmail.com>
2023-12-12 15:14:58 -0500

    Remove unused code and add PlayerController class

--- Assets/Scripts/Dialogue/SetDialogue.cs
+++ Assets/Scripts/Dialogue/SetDialogue.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -17,6 +14,8 @@ public class SetDialogue : MonoBehaviour
     GameObject buttonOption1Object;
     GameObject buttonOption2Object;
     int optionPicked;
+
+    PlayerController player;
 
     // Start is called before the first frame update
     void Start()
